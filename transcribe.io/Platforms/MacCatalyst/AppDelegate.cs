1defcbbe3d6cc3630d001c0b4f380d08042c2d0d
André Pedersen <andrped94@gmail.com>
2025-05-30 22:10:25 +0200

    Configured test app

---
+++ transcribe.io/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace transcribe.io;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
